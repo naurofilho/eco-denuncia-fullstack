7d63247656dfeb16dae98b826412f7aaa1e5ff2f
naurofilho <103686421+naurofilho@users.noreply.github.com>
2026-04-16 10:44:29 -0300

    Delete # models.py

--- # models.py
+++
@@ -1,5 +0,0 @@
-class Denuncia(models.Model):
-    tipo = models.CharField(max_length=100)
-    descricao = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
