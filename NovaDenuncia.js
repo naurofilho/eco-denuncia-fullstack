bd4ac1a4ce3e4ec0a43e5af642f769ce67cafd11
naurofilho <103686421+naurofilho@users.noreply.github.com>
2026-04-16 10:46:32 -0300

    Add NovaDenuncia component for new report form

---
+++ NovaDenuncia.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function NovaDenuncia() {
+  return (
+    <div>
+      <h2>Nova Denúncia</h2>
+      <input placeholder="Tipo" />
+      <textarea placeholder="Descrição"></textarea>
+      <button>Enviar</button>
+    </div>
+  );
+}
+
+export default NovaDenuncia;
