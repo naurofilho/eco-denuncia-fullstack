271cdf7e1ada83130c77ace054e92461511d228a
naurofilho <103686421+naurofilho@users.noreply.github.com>
2026-04-16 10:46:16 -0300

    Create Login.js

---
+++ Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <input placeholder="Email" />
+      <input placeholder="Senha" type="password" />
+      <button>Entrar</button>
+    </div>
+  );
+}
+
+export default Login;
